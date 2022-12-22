8ae9017175f55f889bb36966a82a7e7afb29c4a2
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2022-12-23 00:12:36 +0100

    se crea la funcion operar y lo calcula a mano

--- day-21/src/day_21/core.clj
+++ day-21/src/day_21/core.clj
@@ -1,7 +1,24 @@
 (ns day-21.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.core.logic :as logic])
+  (:require [clojure.core.logic.fd :as fd]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn operar []
+  (logic/run 1 [q]
+             (logic/fresh [root dbpl cczh zczc ptdq dvpt lfqf humn ljgn sjmn sllz pppw lgvd drzm hmdt]
+                          (logic/== q root)
+                          (fd/+ pppw sjmn root)
+                          (logic/== dbpl 5)
+                          (fd/+ sllz lgvd cczh)
+                          (logic/== zczc 2)
+                          (fd/- humn dvpt ptdq)
+                          (logic/== dvpt 3)
+                          (logic/== lfqf 4)
+                          (logic/== humn 5)
+                          (logic/== ljgn 2)
+                          (fd/* drzm dbpl sjmn)
+                          (logic/== sllz 4)
+                          (fd/quot cczh lfqf pppw)
+                          (fd/* ljgn ptdq lgvd)
+                          (fd/- hmdt zczc drzm)
+                          (logic/== hmdt 32))))
