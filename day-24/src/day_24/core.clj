1082dcc928452d188a46ea934d9bbf39839ddf4d
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2022-12-26 20:28:41 +0100

    Programada funcion obtener-objetos

--- day-24/src/day_24/core.clj
+++ day-24/src/day_24/core.clj
@@ -20,6 +20,37 @@
       (get linea)
       (get col)))
 
+; obtener-objetos :: [[A]] -> (Diccionario)
+(defn obtener-objetos [tablero]
+  (let [nlin (count tablero)
+        ncol (count (first tablero))]
+    (loop [indice-lin 0
+           indice-col 0
+           lista-salida '()]
+      (if (= indice-lin nlin)
+       (reverse lista-salida)
+        (let [nueva-linea (if (= indice-col ncol)
+                            (+ indice-lin 1)
+                            indice-lin)
+              nueva-col (if (= indice-col ncol)
+                          0
+                          (+ indice-col 1))
+              elemento (leer-array2D tablero indice-lin indice-col)
+              posicion-objeto (list indice-lin indice-col)
+              direccion-objeto (cond (= elemento "^") (list -1 0)
+                                     (= elemento "v") (list 1 0)
+                                     (= elemento "<") (list 0 -1)
+                                     (= elemento ">") (list 0 1)
+                                     :else (list 0 0))
+              objeto {:pos posicion-objeto, :dir direccion-objeto}
+              nueva-salida (if (or (= elemento "^")
+                                   (= elemento "v")
+                                   (= elemento "<")
+                                   (= elemento ">")) 
+                             (conj lista-salida objeto)
+                             lista-salida)]
+          (recur nueva-linea nueva-col nueva-salida))))))
+
 (defn -main []
   (let [lista-datos (->> "./resources/input.lst"
                          (slurp)
--- day-24/test/day_24/core_test.clj
+++ day-24/test/day_24/core_test.clj
@@ -8,8 +8,19 @@
                [false false]]
         ary-2 [[false false]
                [false false]
-               [false true]]]
+               [false true]]
+        ary-3 [["#" "." "#" "#" "#" "#" "#"] 
+               ["#" "." "." "." "." "." "#"] 
+               ["#" ">" "." "." "." "." "#"] 
+               ["#" "." "." "." "." "." "#"] 
+               ["#" "." "." "." "v" "." "#"] 
+               ["#" "." "." "." "." "." "#"] 
+               ["#" "#" "#" "#" "#" "." "#"]]
+        objetos '({:pos (2 1), :dir (0 1)}
+                  {:pos (4 4), :dir (1 0)})]
     (test/testing "arrays 2D"
       (test/is (= (crear-array2D 3 2 false) ary-1))
       (test/is (= (escribir-array2D ary-1 2 1 true) ary-2))
-      (test/is (= (leer-array2D ary-2 2 1) true)))))
+      (test/is (= (leer-array2D ary-2 2 1) true)))
+    (test/testing "Obtener objetos desde el tablero"
+      (test/is (= (obtener-objetos ary-3) objetos)))))
