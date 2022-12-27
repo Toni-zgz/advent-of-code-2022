360d1f85b20659532f95ed7427af326b25dce880
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2022-12-27 20:03:16 +0100

    Implementamos la función snafu->decimal Hallamos la suma total de los datos de entrada en decimal

--- day-25/src/day_25/core.clj
+++ day-25/src/day_25/core.clj
@@ -1,7 +1,28 @@
 (ns day-25.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as st]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn snafu->decimal [cadena]
+  (loop [cad-bucle cadena
+         salida 0]
+    (if (= cad-bucle "")
+      (/ salida 5) ; para compensar el hecho de que el numero mas a la derecha tiene que
+      ; multiplicarse por 1 y no por 5.
+      (let [elt (str (first cad-bucle))
+            numero (cond (= elt "-") -1
+                         (= elt "=") -2
+                         :else (read-string elt))
+            ; iterativamente multiplicamos por 5 en cada posicion
+            nueva-salida (* (+ salida numero) 5)
+            nueva-cadena (st/join (rest cad-bucle))]
+        (recur nueva-cadena nueva-salida)))))
+
+(defn -main []
+  (let [lista-entrada (->> "./resources/input.lst"
+                           (slurp)
+                           (st/split-lines))
+        tarea-1 (->> lista-entrada
+                     (map #(snafu->decimal %))
+                     (reduce + 0))]
+    (->> tarea-1
+         (println))))
--- day-25/test/day_25/core_test.clj
+++ day-25/test/day_25/core_test.clj
@@ -1,7 +1,21 @@
 (ns day-25.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :as test]
             [day-25.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(test/deftest day25-test
+  (test/testing "Conversion SNAFU a decimal"
+    (test/is (= (snafu->decimal "1=-0-2") 1747))
+    (test/is (= (snafu->decimal "12111") 906))
+    (test/is (= (snafu->decimal "2=0=") 198))
+    (test/is (= (snafu->decimal "21") 11))
+    (test/is (= (snafu->decimal "2=01") 201))
+    (test/is (= (snafu->decimal "111") 31))
+    (test/is (= (snafu->decimal "20012") 1257))
+    (test/is (= (snafu->decimal "112") 32))
+    (test/is (= (snafu->decimal "1=-1=") 353))
+    (test/is (= (snafu->decimal "1-12") 107))
+    (test/is (= (snafu->decimal "12") 7))
+    (test/is (= (snafu->decimal "1=") 3))
+    (test/is (= (snafu->decimal "122") 37))))
+
+ 
