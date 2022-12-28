42242a81b65e9f44e403a883581cfe21853508a5
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2022-12-28 20:28:17 +0100

    Resuelta tarea 1 dia 25

--- day-25/src/day_25/core.clj
+++ day-25/src/day_25/core.clj
@@ -2,6 +2,7 @@
   (:gen-class)
   (:require [clojure.string :as st]))
 
+; snafu->decimal :: String -> Int
 (defn snafu->decimal [cadena]
   (loop [cad-bucle cadena
          salida 0]
@@ -17,12 +18,36 @@
             nueva-cadena (st/join (rest cad-bucle))]
         (recur nueva-cadena nueva-salida)))))
 
+; decimal->snafu :: Int -> String
+(defn decimal->snafu [numero]
+  (loop [numero-bucle numero
+         acarreo 0
+         lista-salida '()]
+    (cond (and (= numero-bucle 0)
+               (= acarreo 0)) (st/join lista-salida)
+          (and (= numero-bucle 0)
+               (= acarreo 1)) (st/join (conj lista-salida "1"))
+          :else (let [nuevo-numero (quot numero-bucle 5)
+                      elt (+ (rem numero-bucle 5) acarreo)
+                      nuevo-acarreo (if (> elt 2)
+                                      1
+                                      0)
+                      nueva-cad (cond (= elt 0) "0"
+                                      (= elt 1) "1"
+                                      (= elt 2) "2"
+                                      (= elt 3) "="
+                                      (= elt 4) "-"
+                                      (= elt 5) "0")
+                      nueva-salida (conj lista-salida nueva-cad)]
+                  (recur nuevo-numero nuevo-acarreo nueva-salida)))))
+
 (defn -main []
   (let [lista-entrada (->> "./resources/input.lst"
                            (slurp)
                            (st/split-lines))
         tarea-1 (->> lista-entrada
                      (map #(snafu->decimal %))
-                     (reduce + 0))]
+                     (reduce + 0)
+                     (decimal->snafu))]
     (->> tarea-1
          (println))))
--- day-25/test/day_25/core_test.clj
+++ day-25/test/day_25/core_test.clj
@@ -16,6 +16,20 @@
     (test/is (= (snafu->decimal "1-12") 107))
     (test/is (= (snafu->decimal "12") 7))
     (test/is (= (snafu->decimal "1=") 3))
-    (test/is (= (snafu->decimal "122") 37))))
+    (test/is (= (snafu->decimal "122") 37)))
+  (test/testing "Conversion decimal a SNAFU"
+    (test/is (= (decimal->snafu 1747) "1=-0-2"))
+    (test/is (= (decimal->snafu 906) "12111"))
+    (test/is (= (decimal->snafu 198) "2=0="))
+    (test/is (= (decimal->snafu 11) "21"))
+    (test/is (= (decimal->snafu 201) "2=01"))
+    (test/is (= (decimal->snafu 31) "111"))
+    (test/is (= (decimal->snafu 1257) "20012"))
+    (test/is (= (decimal->snafu 32) "112"))
+    (test/is (= (decimal->snafu 353) "1=-1="))
+    (test/is (= (decimal->snafu 107) "1-12"))
+    (test/is (= (decimal->snafu 7) "12"))
+    (test/is (= (decimal->snafu 3) "1="))
+    (test/is (= (decimal->snafu 37) "122"))))
 
  
